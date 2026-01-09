dac9cf80b869c7f23f54da06868e4f9af59fcad6
jgalera <juligalera@gmail.com>
2026-01-09 08:29:31 +0100

    borrado de codigo muerto

--- src/core/core.h
+++ src/core/core.h
@@ -187,36 +187,6 @@ void encoder_setup() {
   last_state = (_digitalRead(ROT_B) << 1) | _digitalRead(ROT_A);
   interrupts();
 }
-/*
-class Encoder {
-public:
-  volatile int8_t val = 0;
-  volatile int8_t step = 0;
-  uint8_t last_state;
-  Encoder(){
-    pinMode(ROT_A, INPUT_PULLUP);
-    pinMode(ROT_B, INPUT_PULLUP);
-    PCMSK2 |= (1 << PCINT22) | (1 << PCINT23); // interrupt-enable for ROT_A,
-ROT_B pin changes; see
-https://github.com/EnviroDIY/Arduino-SDI-12/wiki/2b.-Overview-of-Interrupts
-    PCICR |= (1 << PCIE2);
-    last_state = (_digitalRead(ROT_B) << 1) | _digitalRead(ROT_A);
-    sei();
-  }
-  void event(){
-    switch(last_state = (last_state << 4) | (_digitalRead(ROT_B) << 1) |
-_digitalRead(ROT_A)){ //transition  (see:
-https://www.allaboutcircuits.com/projects/how-to-use-a-rotary-encoder-in-a-mcu-based-project/
-) case 0x31: case 0x10: case 0x02: case 0x23: if(step < 0) step = 0; step++;
-if(step >  3){ step = 0; val++; } break; case 0x32: case 0x20: case 0x01: case
-0x13: if(step > 0) step = 0; step--; if(step < -3){ step = 0; val--; } break;
-    }
-  }
-};
-Encoder enc;
-ISR(PCINT2_vect){  // Interrupt on rotary encoder turn
-  enc.event();
-}*/
 
 // I2C communication starts with a START condition, multiple single
 // byte-transfers (MSB first) followed by an ACK/NACK and stops with a STOP
@@ -249,14 +219,6 @@ ISR(TIMER2_COMPA_vect) // Timer2 COMPA interrupt
 }
 
 #pragma GCC pop_options // end of DSP section
-
-/*ISR (TIMER2_COMPA_vect  ,ISR_NAKED) {
-asm("push r24         \n\t"
-    "lds r24,  0\n\t"
-    "sts 0xB4, r24    \n\t"
-    "pop r24          \n\t"
-    "reti             \n\t");
-}*/
 
 #include "../hal/hal.h"
 
